0578feccc3ba72d746d2463addf3aa6f189a6961
Yash Vekaria <yvekaria.09@gmail.com>
2024-03-09 11:03:27 -0800

    Simplified implementation

--- extension/scripts/background.js
+++ extension/scripts/background.js
@@ -1,51 +1,50 @@
 window.tabId = 0;
 const port = 8000;
 
+// Sending data from to my localhost server
+function sendDataToMyServer(path, data) {
+    fetch(`http://localhost:8000/${path}`, {
+        method: "POST",
+        body: JSON.stringify(data),
+        mode: 'cors',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            "Content-Type": "application/json"
+        }
+    }).then(res => {
+        console.log(`${data.function} data sent to server.`)
+    }).catch(err => {
+        console.error(`Error in sending ${data.function}:`, err)
+    });
+}
+
 // Listening for and capturing Network events
 function onEvent(debugId, message, params) {
     if (tabId != debugId.tabId)
         return;
     if (message == "Network.requestWillBeSent") {
         if (!params.request.url.includes('localhost')) {
-            fetch(`http://localhost:${port}/request`, {
-                method: "POST",
-                body: JSON.stringify({
-                    "http_req": params.request.url,
-                    "request_id": params.requestId,
-                    "top_level_url": 0,
-                    "frame_url": params.documentURL,
-                    "resource_type": params.type,
-                    "header": params.request.headers,
-                    "timestamp": params.timestamp,
-                    "frameId": params.frameId,
-                    "call_stack": params.initiator
-                }),
-                mode: 'cors',
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    "Content-Type": "application/json"
-                }
-            }).then(res => {
-                console.log("request data collected.");
+            sendDataToMyServer("request", {
+                "function": "Network.requestWillBeSent",
+                "http_req": params.request.url,
+                "request_id": params.requestId,
+                "top_level_url": 0,
+                "frame_url": params.documentURL,
+                "resource_type": params.type,
+                "header": params.request.headers,
+                "timestamp": params.timestamp,
+                "frameId": params.frameId,
+                "call_stack": params.initiator
             });
         }
     } else if (message == "Network.requestWillBeSentExtraInfo") {
-        fetch(`http://localhost:${port}/requestInfo`, {
-            method: "POST",
-            body: JSON.stringify({
-                "request_id": params.requestId,
-                "cookies": params.associatedCookies,
-                "headers": params.headers,
-                "connectTiming": params.connectTiming,
-                "clientSecurityState": params.clientSecurityState
-            }),
-            mode: 'cors',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                "Content-Type": "application/json"
-            }
-        }).then(res => {
-            console.log("requestInfo data collected.");
+        sendDataToMyServer("request", {
+            "function": "Network.requestWillBeSentExtraInfo",
+            "request_id": params.requestId,
+            "cookies": params.associatedCookies,
+            "headers": params.headers,
+            "connectTiming": params.connectTiming,
+            "clientSecurityState": params.clientSecurityState
         });
     } 
     else if (message == "Network.responseReceived") {
@@ -54,38 +53,20 @@ function onEvent(debugId, message, params) {
         }, "Network.getResponseBody", {
             "requestId": params.requestId
         }, function(response) {
-            fetch(`http://localhost:${port}/response`, {
-                method: "POST",
-                body: JSON.stringify({
-                    "request_id": params.requestId,
-                    "response": params.response,
-                    "resource_type": params.type
-                }),
-                mode: 'cors',
-                headers: {
-                    'Access-Control-Allow-Origin': '*',
-                    "Content-Type": "application/json"
-                }
-            }).then(res => {
-                console.log("response data collected.");
+            sendDataToMyServer("response", {
+                "function": "Network.responseReceived",
+                "request_id": params.requestId,
+                "response": params.response,
+                "resource_type": params.type
             });
         });
     }
     // Capturing execution of any script
     if (message == "Debugger.scriptParsed") {
-        fetch(`http://localhost:${port}/scriptId`, {
-            method: "POST",
-            body: JSON.stringify({
-                "scriptId": params.scriptId,
-                "url": params.url
-            }),
-            mode: 'cors',
-            headers: {
-                'Access-Control-Allow-Origin': '*',
-                "Content-Type": "application/json"
-            }
-        }).then(res => {
-            console.log("scriptId data collected.");
+        sendDataToMyServer("script", {
+            "function": "Network.responseReceived",
+            "scriptId": params.scriptId,
+            "url": params.url
         });
     }
 }
